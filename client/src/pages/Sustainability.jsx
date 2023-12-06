f609bafa2ae0b6bc529d07806f3f037ae577658d
Brochb <brochbaltzer@gmail.com>
2023-12-05 21:26:40 -0700

    updated sustainability page

--- client/src/pages/Sustainability.jsx
+++ client/src/pages/Sustainability.jsx
@@ -11,6 +11,10 @@ const Sustainability = () => {
                     <div>
                         <p>The university of Maryville published <a href="https://online.maryville.edu/blog/what-is-sustainable-shopping/" target="_blank" rel="noopener noreferrer">this</a> article on the matter.</p>
                     </div>
+                    <h4>perchenergy.com's article on sustainable shopping, and eating green:</h4>
+                    <div>
+                        <p>perchenergy.com published <a href="https://www.perchenergy.com/blog/lifestyle/sustainable-grocery-shopping-eating-sustainably" target="_blank" rel="noopener noreferrer">this</a> article on the matter.</p>
+                    </div>
                 </div>
             </div>
         </main>
