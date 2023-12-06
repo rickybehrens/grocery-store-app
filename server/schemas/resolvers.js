2b22aba44d69622dd2531db24c3a52527b9e41c3
Brochb <brochbaltzer@gmail.com>
2023-12-05 18:21:11 -0700

    login

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,16 +2,16 @@ const { User } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        users: async () => {
-            return User.find({});
-        },
-        user: async (parent, { username }) => {
-            return User.findOne({ username });
-        },
+  Query: {
+    users: async () => {
+      return User.find({});
     },
+    user: async (parent, { username }) => {
+      return User.findOne({ username });
+    },
+  },
 
-Mutation: {
+  Mutation: {
     addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
@@ -19,23 +19,25 @@ Mutation: {
     },
 
     login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-      
-        if (!user) {
-          throw new AuthenticationError("User not found");
-        }
-      
-        const correctPw = await user.isCorrectPassword(password);
-      
-        if (!correctPw) {
-          throw new AuthenticationError("Incorrect password");
-        }
-      
-        const token = signToken(user);
-      
-        return { token, user };
-      }}};
-      
-    
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
+  }
+};
+
+
 module.exports = resolvers;
 
