efdac3deb444f413412ffb218f732de988ff10ff
Brochb <brochbaltzer@gmail.com>
2023-12-09 16:14:18 -0700

    added space between darkmode button and page content, first version of the shopping list finally functioning

--- client/src/pages/ShoppingList.jsx
+++ client/src/pages/ShoppingList.jsx
@@ -1,106 +1,44 @@
 import React from 'react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import { useMutation } from '@apollo/client';
-import { ADD_ITEM } from '../utils/mutations';
 
 const Shoppinglist = () => {
-    const [formState, setFormState] = useState({
-        itemname: ''
-    })
     const [items, setItems] = useState([]);
 
-    const [addItem, { error, data }] = useMutation(ADD_ITEM);
+    const [inputValue, setInputValue] = useState('');
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-
-        setFormState({
-            ...formState,
-            [name]: value,
-        });
-    };
-
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
-        console.log(formState);
-
-        try {
-            const { data } = await addItem({
-                variables: { ...formState },
-            });
-
-            console.log('(DELETE BEFORE PRODUCTION)Mutation Response:', data)
-
-            const newItem = data.addItem.item;
-
-            // Updates the list of items
-            setItems([...items, newItem]);
-
-            // Clears the users input
-            setFormState({
-                itemname: ''
-            });
-
-        } catch (e) {
-            console.error('GraphQL error:', e);
+    const addItem = () => {
+        if (inputValue.trim() !== '') {
+            setItems([...items, inputValue]);
+            setInputValue('');
         }
     };
 
-    const handleItemClick = (itemId) => {
-        const updatedItems = items.filter((item) => item.id !== itemId);
+    const deleteItem = (index) => {
+        const updatedItems = [...items];
+        updatedItems.splice(index, 1);
         setItems(updatedItems);
     };
 
-
     return (
-        <main className="flex-row justify-center mb-4">
-            <div className="col-12 col-lg-10">
-                <div className="card">
-                    <h4 className="card-header bg-dark text-light p-2">Shopping List</h4>
-                    <div className="card-body">
-                        {data ? (
-                            <p>
-                                Success! You may now head{' '}
-                                <Link to="/">back to the homepage.</Link>
-                            </p>
-                        ) : (
-                            <form onSubmit={handleFormSubmit}>
-                                <input
-                                    className="form-input"
-                                    placeholder="Item Name"
-                                    name="itemname"
-                                    type="text"
-                                    value={formState.itemname}
-                                    onChange={handleChange}
-                                />
-                                <button
-                                    className="btn btn-block btn-primary"
-                                    style={{ cursor: 'pointer' }}
-                                    type="submit"
-                                >
-                                    Submit
-                                </button>
-                            </form>
-                        )}
-
-                        {error && (
-                            <div className="my-3 p-3 bg-danger text-white">
-                                {error.message}
-                            </div>
-                        )}
-                    </div>
-                    <ul>
-                        {items.map((item) => (
-                            <li key={item.id} onClick={() => handleItemClick(item.id)}>
-                                {item.itemname}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+        <div>
+            <h1>Shopping List</h1><br></br>
+            <div>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="Enter Item Name"
+                />
+                <button onClick={addItem}>Add Item</button>
             </div>
-        </main>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index} onClick={() => deleteItem(index)}>
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
--- client/src/pages/Sustainability.jsx
+++ client/src/pages/Sustainability.jsx
@@ -9,11 +9,11 @@ const Sustainability = () => {
                 <div>
                     <h4>Maryville University's article on sustainable shopping:</h4>
                     <div>
-                        <p>The university of Maryville published <a href="https://online.maryville.edu/blog/what-is-sustainable-shopping/" target="_blank" rel="noopener noreferrer">this</a> article on the matter.</p>
+                        <p>The university of Maryville published <a href="https://online.maryville.edu/blog/what-is-sustainable-shopping/" target="_blank" rel="noopener noreferrer">THIS</a> article on the matter.</p>
                     </div>
                     <h4>perchenergy.com's article on sustainable shopping, and eating green:</h4>
                     <div>
-                        <p>perchenergy.com published <a href="https://www.perchenergy.com/blog/lifestyle/sustainable-grocery-shopping-eating-sustainably" target="_blank" rel="noopener noreferrer">this</a> article on the matter.</p>
+                        <p>perchenergy.com published <a href="https://www.perchenergy.com/blog/lifestyle/sustainable-grocery-shopping-eating-sustainably" target="_blank" rel="noopener noreferrer">THIS</a> article on the matter.</p>
                     </div>
                 </div>
             </div>
