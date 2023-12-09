d0d48bdfee19010425cb471db352c5cdab61fba1
Ricardo Behrens <behrensricardo@gmail.com>
2023-12-09 15:33:02 -0700

    Got the map rendered and now working on adding grogery stores near me

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,24 +11,36 @@ const typeDefs = `
     user: User
   }
 
-  type Product {
+  type Coordinates {
+    type: String
+    coordinates: [Float]
+  }
+
+  type LocationProperties {
+    country: String
+    locality: String
+    borough: String
+    # Add other properties as needed
+  }
+
+  type Location {
     id: ID
     name: String
-    lat: Float!
-    long: Float!
+    coordinates: Coordinates
+    properties: LocationProperties
   }
 
   type Query {
     users: [User]
     user(username: String!): User
-    products(lat: Float!, long: Float!): [Product]  # Add lat and long arguments here
+    products(lat: Float!, long: Float!): [Location]
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     addItem(itemname: String!): ItemResponse  # Update this line to include the mutation
-    updateProducts(lat: Float!, long: Float!): [Product]  # Define the mutation in the schema
+    updateProducts(lat: Float!, long: Float!): [Location]  # Define the mutation in the schema
   }
 
   type Item {
