43107d97326e5cb9bd32f828f7f2f246e53d0c19
Brochb <brochbaltzer@gmail.com>
2023-12-12 20:51:46 -0700

    added error message to signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -86,7 +86,7 @@ const Signup = () => {
             )}
 
             {error && (
-              <div className="my-3 p-3 bg-danger text-white">
+              <div className="my-3 p-3 bg-danger text-red">
                 {error.message}
               </div>
             )}
