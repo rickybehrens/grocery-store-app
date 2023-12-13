1371021fe0a0f194f4361c10ba908ec3d502c862
Ricardo Behrens <behrensricardo@gmail.com>
2023-12-12 21:21:55 -0700

    Added pictures on Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,34 +4,24 @@ const Home = () => {
     return (
         <div>
             Home Page Content
+            <div className='image-grid'>
+                <img src="./images/dinner1.jpeg" alt="" srcset="" />
+                <img src="./images/dinner2.jpeg" alt="" srcset="" />
+                <img src="./images/dinner3.jpeg" alt="" srcset="" />
+                <img src="./images/christmas.jpeg" alt="" srcset="" />
+                <img src="./images/family.jpeg" alt="" srcset="" />
+                <img src="./images/grilling1.jpeg" alt="" srcset="" />
+                <img src="./images/paella.jpeg" alt="" srcset="" />
+                <img src="./images/pizza.jpeg" alt="" srcset="" />
+                <img src="./images/potluck.jpeg" alt="" srcset="" />
+                <img src="./images/smoker.jpeg" alt="" srcset="" />
+                <img src="./images/thanksgiving.jpeg" alt="" srcset="" />
+                <img src="./images/vegetables.jpeg" alt="" srcset="" />
+                <img src="./images/burger.jpeg" alt="" srcset="" />
+                <img src="./images/kitchen.jpeg" alt="" srcset="" />
+                <img src="./images/cheese.jpeg" alt="" srcset="" />
+            </div>
         </div>
     )
 };
 export default Home;
-
-// function Example() {
-//     return (
-//         <>
-//             <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-//                 <div className="text-center">
-//                     <p className="text-base font-semibold text-indigo-600">404</p>
-//                     <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
-//                     <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
-//                     <div className="mt-10 flex items-center justify-center gap-x-6">
-//                         <a
-//                             href="#"
-//                             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-//                         >
-//                             Go back home
-//                         </a>
-//                         <a href="#" className="text-sm font-semibold text-gray-900">
-//                             Contact support <span aria-hidden="true">&rarr;</span>
-//                         </a>
-//                     </div>
-//                 </div>
-//             </main>
-//         </>
-//     )
-// }
-
-// export { Example };
